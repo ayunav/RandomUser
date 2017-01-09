31254438e4268093542f35050a44d50bbac1b324
Ayuna NYC <ayunav@users.noreply.github.com>
2017-01-09 15:18:10 -0500

    Set up user table view cell

---
+++ UserTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  UserTableViewCell.swift
+//  RandomUser
+//
+//  Created by Ayuna NYC on 1/9/17.
+//  Copyright © 2017 Ayuna NYC. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var avatarView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
