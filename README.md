687a4199f16e071de539422177f75dd4e09b7055
Ayuna NYC <ayunav@users.noreply.github.com>
2017-01-10 19:56:39 -0500

    Rename ViewController class to UsersViewController

--- README.md
+++ README.md
@@ -1,19 +1,36 @@
 # RandomUser
 
 
-- load data from JSON file at path 
-- The JSON file should be loaded from the network when the app starts. Display an appropriate loading indicator as it loads. 
 
-- The thumbnail should be loaded in the background, and be stored on the local file system after it has been retrieved from the network.
-- If a thumbnail has already be loaded from the network and is available from the local file system, then it should load from the local file system. 
+
+- the thumbnail should be stored on the local file system after it has been retrieved from the network.
+- If a thumbnail has already been loaded from the network and is available from the local file system, then it should load from the local file system. 
+
 - Animate the display of the thumbnail into the table view cell (use whatever animation you’d like). 
-- Only the thumbnail for each visible cell should loaded (i.e. don’t preemptively load all thumbnails from the network on the view appearing). 
 
-- add nav controller with nav bar. nav bar title - random user  
+
+
+- Only the thumbnail for each visible cell should be loaded (i.e. don’t preemptively load all thumbnails from the network on the view appearing). 
+
+
+TO-DO: 
+
 - customize layout of the table view cell 
 - Optionally make the app look as slick as you’d like (use your judgement here).
+- The JSON file should be loaded from the network when the app starts. Display an appropriate loading indicator as it loads. 
+- add placeholder image 
+- add vc programmatically
+- increase name label font 
 
 Resources: 
+
+- https://swiftunboxed.com/lang/closures-escaping-noescape-swift3/
 - http://swiftdeveloperblog.com/code-examples/create-uitableview-programmatically-example-in-swift/
+- http://www.appcoda.com/swift3-changes/
 
+Qs: 
 
+- naming - dataprovider?? 
+- app transport security, http links
+- http://stackoverflow.com/questions/30731785/how-do-i-load-an-http-url-with-app-transport-security-enabled-in-ios-9
+- https://developer.apple.com/library/content/documentation/General/Reference/InfoPlistKeyReference/Articles/CocoaKeys.html (app transport security)
---
+++ RandomUser/UsersViewController.swift
@@ -0,0 +1,98 @@
+//
+//  UsersViewController.swift
+//  RandomUser
+//
+//  Created by Ayuna NYC on 1/8/17.
+//  Copyright © 2017 Ayuna NYC. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    // MARK: - Properties
+    
+    private let CellIdentifier = "UserTableViewCellIdentifier"
+    
+    var tableView = UITableView()
+
+    var users = [User]()
+    
+    let userStore = UserStore()
+    
+    
+    // MARK: - View
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTableView()
+        
+        userStore.getUsers() { (usersResult) in
+            OperationQueue.main.addOperation {
+                // add loading indicator
+                switch usersResult {
+                case let .success(_users):
+                    self.users = _users
+                    self.tableView.reloadData()
+                case .failure():
+                    print("\n \n \n Error fetching users \n \n \n")
+                }
+            }
+        }
+    }
+
+    
+    // MARK: - UITableView
+    
+    func setupTableView() {
+        
+        let screenSize = UIScreen.main.bounds
+        tableView.frame = CGRect(x: 0, y: 20, width: screenSize.width, height: screenSize.height)
+                
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UINib(nibName: "UserTableViewCell", bundle: nil), forCellReuseIdentifier: CellIdentifier)
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 80
+        
+        view.addSubview(tableView)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    
+//    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//        
+//        let user = users[indexPath.row]
+//        
+//        userStore.requestImageForUser(user: user, completion: { (imageResult) -> Void in
+//        
+//            OperationQueue.main.addOperation {
+//            
+//                if let userCell = cell as? UserTableViewCell {
+//                
+//                    userCell.updateWithImage(image: user.avatar)
+//                }
+//            }
+//        })
+//    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier, for: indexPath) as! UserTableViewCell
+        
+        let user = users[indexPath.row]
+
+        cell.avatarView.image = user.avatar
+        cell.nameLabel.text = user.name
+        
+        return cell
+    }
+
+}
