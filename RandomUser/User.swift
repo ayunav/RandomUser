67e52b9c14f5a9e11195e53ac78f5b96305cd3a8
Ayuna NYC <ayunav@users.noreply.github.com>
2017-01-10 19:57:06 -0500

    User model changes

--- RandomUser/User.swift
+++ RandomUser/User.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
+
+enum UserFields: String
+{
+    case id        = "id"
+    case name      = "name"
+    case thumbnail = "thumbnail"
+}
+
+
 struct User {
     
-    let avatar: UIImage
-    let name: String
+    var avatar      : UIImage
+    let id          : Int
+    let name        : String
+    let thumbnailUrl: URL
+    
 
-    init(avatar: UIImage, name: String) {
+    init(avatar: UIImage, id: Int, name: String, thumbnailUrl: URL) {
         self.avatar = avatar
+        self.id = id
         self.name = name
+        self.thumbnailUrl = thumbnailUrl
+    }
+    
+    
+    static func userFromJsonDict(json: [String: Any]) -> User?
+    {
+        guard let id = json[UserFields.id.rawValue] as? Int,
+            let name = json[UserFields.name.rawValue] as? String,
+            let thumbnailUrlString = json[UserFields.thumbnail.rawValue] as? String,
+            let url = URL(string: thumbnailUrlString)
+        else { return nil }
+
+        let data = try! Data(contentsOf: url, options: [])
+        
+        var image = UIImage()
+        
+        if data.count > 0 {
+            image = UIImage(data: data)!
+        } else {
+            image = UIImage(named: "placeholder.png")!
+        }
+
+        return User(avatar: image, id: id, name: name, thumbnailUrl: url)
     }
 }
